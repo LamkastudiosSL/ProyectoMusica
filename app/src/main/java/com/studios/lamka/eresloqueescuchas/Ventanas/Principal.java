090ac10902516ec36e68cb4dcc4062f67b90f108
jab95 <josejab95@gmail.com>
2018-04-05 22:57:30 +0200

    SSSS

--- app/src/main/java/com/studios/lamka/eresloqueescuchas/Ventanas/Principal.java
+++ app/src/main/java/com/studios/lamka/eresloqueescuchas/Ventanas/Principal.java
@@ -25,7 +25,7 @@ public class Principal extends AppCompatActivity implements View.OnClickListener
         encuesta.setOnClickListener(this);
         acerca.setOnClickListener(this);
 
-        //ESTA ES LA ULTIMAAAAA YAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        //ESTA ES LA ULTIMAAAAA YAAAAAAAAAAAAAAAAAAAAAAA
     }
 
 
