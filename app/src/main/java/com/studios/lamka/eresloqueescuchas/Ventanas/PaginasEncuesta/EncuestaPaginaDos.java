34f185151cacd6f0f266f108cc9c55ed3765a752
jab95 <josejab95@gmail.com>
2018-04-08 14:50:27 +0200

    actualizada estructura vista pagina 1

--- app/src/main/java/com/studios/lamka/eresloqueescuchas/Ventanas/PaginasEncuesta/EncuestaPaginaDos.java
+++ app/src/main/java/com/studios/lamka/eresloqueescuchas/Ventanas/PaginasEncuesta/EncuestaPaginaDos.java
@@ -3,6 +3,7 @@ package com.studios.lamka.eresloqueescuchas.Ventanas.PaginasEncuesta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -49,6 +50,8 @@ public class EncuestaPaginaDos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paginados);
+        //AL PULSAR UN EDDITEXT TE ARREGLA EL FALLO QUE SE TE MUEVE EL LAYOUT
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         btnSiguiente = findViewById(R.id.btnsiguiente);
         btnAtras = findViewById(R.id.btnpaginanaterior);
